445489bcfea8cb6c7ab4bafa1d3e5a38ee3f1957
kalai-git-dev <mac@MacBook-Pro-de-mac.local>
2021-01-26 20:41:54 +0100

    add basket

--- src/components/Asside.js
+++ src/components/Asside.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Asside({ data, paniers, setPaniers }) {
+function Asside({ paniers, setPaniers }) {
   let total = 0;
 
   for (let i = 0; i < paniers.length; i++) {
@@ -16,51 +16,57 @@ function Asside({ data, paniers, setPaniers }) {
       <div className="asside">
         <button className="asside-button">Valider mon panier</button>
         {paniers.length === 0 ? (
-          <p>le panier est vide !!</p>
+          <p className="panier-empty">le panier est vide !!</p>
         ) : (
           <div>
             {paniers.map((item, index) => {
               return (
                 <>
-                  <div key={item.id}>
-                    <button
-                      className="less"
-                      onClick={() => {
-                        const newPaniers = [...paniers];
-                        // console.log(newPaniers[index]);
-                        if (newPaniers[index].quantity === 1) {
-                          newPaniers.splice(index, 1);
-                          setPaniers(newPaniers);
-                        } else {
-                          newPaniers[index].quantity--;
-                          setPaniers(newPaniers);
-                        }
-                      }}
-                    >
-                      -
-                    </button>
-                    <span>{item.quantity}</span>
-                    <button
-                      className="more"
-                      onClick={() => {
-                        const newPaniers = [...paniers];
-                        // console.log(newPaniers[index]);
+                  <div key={index} id="product" key={item.id}>
+                    <div className="bloc-icons">
+                      <button
+                        className="less"
+                        onClick={() => {
+                          const newPaniers = [...paniers];
+                          // console.log(newPaniers[index]);
+                          if (newPaniers[index].quantity === 1) {
+                            newPaniers.splice(index, 1);
+                            setPaniers(newPaniers);
+                          } else {
+                            newPaniers[index].quantity--;
+                            setPaniers(newPaniers);
+                          }
+                        }}
+                      >
+                        -
+                      </button>
+                      <span>{item.quantity}</span>
+                      <button
+                        className="more"
+                        onClick={() => {
+                          const newPaniers = [...paniers];
+                          // console.log(newPaniers[index]);
 
-                        newPaniers[index].quantity++;
-                        setPaniers(newPaniers);
-                      }}
-                    >
-                      +
-                    </button>
-                    <span>{item.title}</span>
-                    <span>{item.price} £ </span>
+                          newPaniers[index].quantity++;
+                          setPaniers(newPaniers);
+                        }}
+                      >
+                        +
+                      </button>
+                    </div>
+                    <div className="bloc-rigth">
+                      <span className="product-title">{item.title}</span>
+                      <span className="product-price">
+                        {(Number(item.price) * item.quantity).toFixed(2)} £
+                      </span>
+                    </div>
                   </div>
                 </>
               );
             })}
           </div>
         )}
-        <p>{total.toFixed(2)} £</p>
+        <p className="total">Total :{total.toFixed(2)} £</p>
       </div>
     </>
   );
